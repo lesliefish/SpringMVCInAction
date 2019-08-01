aa383f32cc825f8380ac622c1c49f7e6e00c6f6c
leiyu <yuleics@outlook.com>
2019-08-01 10:44:04 +0800

    static page 静态页面重定向

---
+++ SpringMVC/src/com/lesliefish/formhandling/StaticPageController.java
@@ -0,0 +1,13 @@
+package com.lesliefish.formhandling;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class StaticPageController {
+    @RequestMapping(value = "/staticPageIndex", method = RequestMethod.GET)
+    public String index(){
+        return "redirect:/pages/staticPage.html";
+    }
+}
